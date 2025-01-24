a17d1567b41b6c783df1b64fcd35b13dc802502f
Leandro Amaral <lramaral02m@gmail.com>
2025-01-24 11:41:44 -0300

    Get Game by ID

--- src/main/java/org/lehdev/game_list/services/GameService.java
+++ src/main/java/org/lehdev/game_list/services/GameService.java
@@ -1,10 +1,12 @@
 package org.lehdev.game_list.services;
 
+import org.lehdev.game_list.dto.GameDTO;
 import org.lehdev.game_list.dto.GameMinDTO;
 import org.lehdev.game_list.entities.Game;
 import org.lehdev.game_list.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,6 +16,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         var result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();

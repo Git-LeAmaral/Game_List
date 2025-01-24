b138c877ea24fd8b4ec8c4ac3514bca40ec8ed44
Leandro Amaral <lramaral02m@gmail.com>
2025-01-24 14:09:40 -0300

    SQL queries in spring jpa data

--- src/main/java/org/lehdev/game_list/dto/GameMinDTO.java
+++ src/main/java/org/lehdev/game_list/dto/GameMinDTO.java
@@ -1,7 +1,7 @@
 package org.lehdev.game_list.dto;
 
-import jakarta.persistence.Column;
 import org.lehdev.game_list.entities.Game;
+import org.lehdev.game_list.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -19,6 +19,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
--- src/main/java/org/lehdev/game_list/services/GameService.java
+++ src/main/java/org/lehdev/game_list/services/GameService.java
@@ -3,6 +3,7 @@ package org.lehdev.game_list.services;
 import org.lehdev.game_list.dto.GameDTO;
 import org.lehdev.game_list.dto.GameMinDTO;
 import org.lehdev.game_list.entities.Game;
+import org.lehdev.game_list.projections.GameMinProjection;
 import org.lehdev.game_list.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,5 +30,11 @@ public class GameService {
         var result = gameRepository.findAll();
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
